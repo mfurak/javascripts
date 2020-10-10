32db2020cb1abb662e473e030d460a48bb2d23f8
mfurak <38523093+mfurak@users.noreply.github.com>
2020-10-10 06:22:08 +0200

    Added couple of YT scripts

---
+++ scripts.js
@@ -0,0 +1,28 @@
+//create a prompt that asks for custom speed for YT playback
+(function() {
+  var isYT = window.location.href.indexOf("youtube.com");
+  if (isYT != -1) {
+    var playbackSpeed = prompt("Choose desired speed");
+    document.getElementsByTagName("video")[0].playbackRate = playbackSpeed;
+  }
+})();
+
+//remove YT consent dialog
+(function() {
+  var e = document.getElementById("consent-bump");
+  if (e) {
+    e.parentComponent.removeChild(e);
+  }
+})();
+
+// use the NSFW version of YT (taken from the site)
+(function() {
+  var originalBaseURL = window.location.href;
+  var isyt = originalBaseURL.indexOf("youtube.com");
+  if (isyt != -1 && originalBaseURL.indexOf("nsfwyoutube.com") == -1) {
+    var http_url = originalBaseURL.replace("youtube.com", "nsfwyoutube.com");
+    window.location = http_url;
+  } else {
+    alert("You have to be on youtube.com for this to work!");
+  }
+})();
