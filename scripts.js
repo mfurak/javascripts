097991ab1209a0ccce01aed74d82e47c819d3fdf
mfurak <marek.furak@gmail.com>
2023-05-04 00:58:19 +0200

    format

--- scripts.js
+++ scripts.js
@@ -1,21 +1,20 @@
 //prompt that asks for custom speed for YT playback
 // also removes YT consent dialog(might not be needed since YT doesn't use it anymore)
 (function () {
-  var isYT = window.location.href.indexOf('youtube.com');
+  var isYT = window.location.href.indexOf("youtube.com");
   if (isYT !== -1) {
-    var e = document.getElementById('consent-bump');
+    var e = document.getElementById("consent-bump");
     if (e) {
       e.parentComponent.removeChild(e);
     }
-    var playbackSpeed = prompt('Choose desired speed');
-    document.getElementsByTagName('video')[0].playbackRate = playbackSpeed;
-    document.getElementsByTagName('video')[0].play();
+    var playbackSpeed = prompt("Choose desired speed");
+    document.getElementsByTagName("video")[0].playbackRate = playbackSpeed;
+    document.getElementsByTagName("video")[0].play();
   }
 })();
 
-
 // use the NSFW version of YT (taken from the site)
-(function() {
+(function () {
   var originalBaseURL = window.location.href;
   var isyt = originalBaseURL.indexOf("youtube.com");
   if (isyt != -1 && originalBaseURL.indexOf("nsfwyoutube.com") == -1) {
